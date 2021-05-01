c1a60c591ff8906755e11528679079bc0946d506
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-05-01 19:33:53 +0200

    fetch user on /me depending on local storage JWT value, basic really

--- server/routes/privateTest.js
+++ server/routes/privateTest.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 
 router.get("/", async (req, res) => {
+
     const token = req.headers.authorization.split(" ")[1];
-    
     const { _id } = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
 
     const user = await User.findOne({
