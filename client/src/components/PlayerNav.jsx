af9f31a7bf54a2168ce727885b3e276a268912f6
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-05-13 18:27:34 +0200

    player profile navbar

---
+++ client/src/components/PlayerNav.jsx
@@ -0,0 +1,19 @@
+const PlayerNav = () => {
+  return ( 
+      <nav className="profile-navbar">
+        <div className="profile-navbar-wrapper">
+          <a href="" className="overview">
+            <p>OVERVIEW</p>
+          </a>
+          <a href="" className="champions">
+            <p>CHAMPION STATS</p>
+          </a>
+          <a href="" className="live-game">
+            <p>LIVE GAME</p>
+          </a>
+        </div>
+      </nav>
+    );
+}
+ 
+export default PlayerNav;
--- client/src/components/PlayerStats.jsx
+++ client/src/components/PlayerStats.jsx
@@ -1,11 +1,13 @@
 import PlayerCard from "./PlayerCard";
 import PlayerChampions from "./PlayerChampions";
+import PlayerNav from "./PlayerNav";
 import PlayerRank from "./PlayerRank";
 
 const PlayerStats = () => {
   return ( 
     <div className="player-stats">
       <PlayerCard />
+      <PlayerNav />
       <PlayerRank />
       <PlayerChampions />
     </div>
