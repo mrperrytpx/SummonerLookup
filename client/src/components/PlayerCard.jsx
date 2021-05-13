c3c18e6323e39c6578fcfdc0df82f9022320bdf0
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-05-13 17:58:39 +0200

    responsive player card

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,12 +1,16 @@
 const PlayerCard = () => {
   return ( 
-    <div className="player-card">
-      <div className="player-icon">
+    <div className="summoner">
+      <div className="summoner-icon">
         <img className="player-summoner-icon" src="https://opgg-static.akamaized.net/images/profile_icons/profileIcon898.jpg" alt="" />
       </div>
-      <div className="player-name-wrap">
-        <div className="player-name">SUMMONER NAME</div> 
-        <button className="follow">FOLLOW</button>  
+      <div className="name-wrapper">
+        <div className="player-name">
+          <p className="summoner-name">hide on bush</p>
+        </div> 
+        <div className="follow-summoner"> 
+          <button className="follow">FOLLOW</button>  
+        </div>
       </div>  
     </div>
     );
