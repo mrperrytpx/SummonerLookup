effd8b772fd73ee8dd5e5b62a08e2b3b914c783c
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-05-20 19:15:48 +0200

    removed all the state from Match cards since all of it was in context state anyway

--- client/src/components/Match.jsx
+++ client/src/components/Match.jsx
@@ -1,77 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import MatchDetails from "./MatchDetails";
 
 const Match = ( { game } ) => {
-
   const [isClicked, setIsClicked] = useState(false);
-  const [assists, setAssists] = useState("");
-  const [deaths, setDeaths] = useState("");
-  const [kills, setKills] = useState("");
-  const [win, setWin] = useState();
-  const [kda, setKda] = useState(0);
-  const [item1, setItem1] = useState("");
-  const [item2, setItem2] = useState("");
-  const [item3, setItem3] = useState("");
-  const [item4, setItem4] = useState("");
-  const [item5, setItem5] = useState("");
-  const [item6, setItem6] = useState("");
-  const [item7, setItem7] = useState("");
-  const [championIcon, setChampionicon] = useState("");
-  const [spell1, setSpell1] = useState("");
-  const [spell2, setSpell2] = useState("");
-  const [keystone, setKeystone] = useState("");
-  const [secondary, setSecondary] = useState("");
-
-  useEffect(() => {
-    setChampionicon(`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/champion/${game?.championName}.png`)
-    setAssists(game?.assists);
-    setDeaths(game?.deaths);
-    setKills(game?.kills);
-    setWin(game?.win);
-    setKda(() => {
-      if (game?.deaths === 0) {
-        return game?.kills + game?.assists
-      } else {
-        return +((game?.kills + game?.assists) / game?.deaths).toFixed(2);
-      }  
-    });
-    setItem1(game?.item0 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item0}.png` : "");
-    setItem2(game?.item1 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item1}.png` : "");
-    setItem3(game?.item2 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item2}.png` : "");
-    setItem4(game?.item3 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item3}.png` : "");
-    setItem5(game?.item4 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item4}.png` : "");
-    setItem6(game?.item5 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item5}.png` : "");
-    setItem7(game?.item6 ? `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item6}.png` : "");
-    setSpell1(`https://ddragon.leagueoflegends.com/cdn/11.10.1/img/spell/${game?.summoner1}.png`);
-    setSpell2(`https://ddragon.leagueoflegends.com/cdn/11.10.1/img/spell/${game?.summoner2}.png`);
-    setKeystone(`https://ddragon.leagueoflegends.com/cdn/img/${game?.keystone}`);
-    setSecondary(`https://ddragon.leagueoflegends.com/cdn/img/${game?.secondary}`);
-  }, [game]);
-
 
   return ( 
     <div 
       onClick={() => setIsClicked(() => !isClicked)} 
-      className={`games-wrapper ${win ? "winner" : "loser"}` }>
+      className={`games-wrapper ${game?.win ? "winner" : "loser"}` }>
 
       <div className="game-wrapper">
 
         <div className="game-champ-setup">
           <div className="game-champ-icon">
-            <img className="ch" src={championIcon} alt="Champion"/>
+            <img className="ch" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/champion/${game?.championName}.png`} alt="Champion"/>
           </div>
           <div className="game-champ-summs">
             <div className="summoner1">
-              <img className="small" src={spell1} alt="s"/>
+              <img className="small" src={`https://ddragon.leagueoflegends.com/cdn/11.10.1/img/spell/${game?.summoner1}.png`} alt="s"/>
             </div>
             <div className="summoner2">
-              <img className="small" src={spell2} alt="s"/>
+              <img className="small" src={`https://ddragon.leagueoflegends.com/cdn/11.10.1/img/spell/${game?.summoner2}.png`} alt="s"/>
             </div>
             <div className="keystone">
-              <img className="small" src={keystone} alt="s"/>
+              <img className="small" src={`https://ddragon.leagueoflegends.com/cdn/img/${game?.keystone}`} alt="s"/>
             </div>
             <div className="secondary">
-              <img className="small" src={secondary} alt="s"/>
+              <img className="small" src={`https://ddragon.leagueoflegends.com/cdn/img/${game?.secondary}`} alt="s"/>
             </div>
           </div>
         </div>
@@ -80,38 +35,43 @@ const Match = ( { game } ) => {
 
           <div className="game-champ-score">
             <p className="score-header">Score:</p>
-            <p className="calculated-kda">{kda} KDA</p>
-            <p className="game-score">{kills} / {deaths} / {assists}</p>
+            <p className="calculated-kda">{() => {
+              if (game?.deaths === 0) {
+                return game?.kills + game?.assists
+              } else {
+                return +((game?.kills + game?.assists) / game?.deaths).toFixed(2);
+              }}} KDA</p>
+            <p className="game-score">{game?.kills} / {game?.deaths} / {game?.assists}</p>
           </div>
 
           <div className="game-champ-build">
             <div className="item1 item">
-              {game?.item0 ? <img className="item-image" src={item1} alt="Item slot 5"/> : <div></div> }            
+              {game?.item0 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item0}.png`} alt="Item slot 5"/> : <div></div> }            
             </div>
             <div className="item2 item">
-              {game?.item1 ? <img className="item-image" src={item2} alt="Item slot 5"/> : <div></div> }            
+              {game?.item1 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item1}.png`} alt="Item slot 5"/> : <div></div> }            
             </div>
             <div className="item3 item">
-              {game?.item2 ? <img className="item-image" src={item3} alt="Item slot 5"/> : <div></div> }            
+              {game?.item2 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item2}.png`} alt="Item slot 5"/> : <div></div> }            
             </div>
             <div className="item4 item">
-              {game?.item3 ? <img className="item-image" src={item4} alt="Item slot 5"/> : <div></div> }            
+              {game?.item3 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item3}.png`} alt="Item slot 5"/> : <div></div> }            
             </div>
             <div className="item5 item">
-              {game?.item4 ? <img className="item-image" src={item5} alt="Item slot 5"/> : <div></div> }            
+              {game?.item4 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item4}.png`} alt="Item slot 5"/> : <div></div> }            
             </div>
             <div className="item6 item">
-              {game?.item5 ? <img className="item-image" src={item6} alt="Item slot 5"/> : <div></div> }
+              {game?.item5 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item5}.png`} alt="Item slot 5"/> : <div></div> }
             </div>
             <div className="item7 item">
-              {game?.item6 ? <img className="item-image" src={item7} alt="Trinket slot"/> : <div></div> }            
+              {game?.item6 ? <img className="item-image" src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/item/${game?.item6}.png`} alt="Trinket slot"/> : <div></div> }            
             </div>
           </div>
           
         </div>
 
       </div>
-      { isClicked && <MatchDetails win={win} id={game?.matchId} /> }
+      { isClicked && <MatchDetails win={game?.win} id={game?.matchId} /> }
     </div>
    );
 }
