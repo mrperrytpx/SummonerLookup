2e3ccbadbe9b716b4ea0da5dfe487e1f3480b95d
mrperrytpx <tomislav.siprak1996@gmail.com>
2022-12-03 10:38:52 +0100

    NO UNUSED VARS

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { StyledHome } from "./Home.styled";
 import { ReactComponent as SquareLogo } from "../../../assets/square_logo_no_text.svg";
 import { CompactSearchSummoner } from "../../organisms/CompactSearchSummoner/CompactSearchSummoner";
-import { Footer } from "../../organisms/Footer/Footer";
 import { SearchSummoner } from "../../organisms/SearchSummoner/SearchSummoner";
 import { useScreenSize } from "../../../hooks/useScreenSize";
 import { Background } from "components/atoms/Background/Background";
