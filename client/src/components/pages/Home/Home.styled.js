a093c52a697fadd2dbfba22b72bc0a12a30c6675
mrperrytpx <tomislav.siprak1996@gmail.com>
2022-12-06 18:43:22 +0100

    min heigt so it loosk better on landsccape

--- client/src/components/pages/Home/Home.styled.js
+++ client/src/components/pages/Home/Home.styled.js
@@ -3,7 +3,7 @@ import { FlexColStart } from "../../atoms/FlexBoxes/FlexBoxes.styled";
 
 export const StyledHome = styled(FlexColStart).attrs({ as: "main" })`
     justify-content: ${(props) => props.center ? "center" : "flex-start"};
-    min-height: calc(100vh - 1rem - 60px); 
+    min-height: calc(max(600px, 100vh) - 1rem - 60px); 
     margin: auto;
     gap: 2rem;
     z-index: 2;
