11fadad32a19901c0a842d5df2db1d5bc1e9d1e5
mrperrytpx <tomislav.siprak1996@gmail.com>
2022-12-08 15:55:00 +0100

    removed jwt-decode, added U.gg disclaimer

--- client/src/components/atoms/Disclaimer/Disclaimer.jsx
+++ client/src/components/atoms/Disclaimer/Disclaimer.jsx
@@ -1,10 +1,14 @@
 import { StyledDisclaimer } from "./Disclaimer.styled";
+import { ReactComponent as UggLogo } from "assets/ugg.svg";
 
 export const Disclaimer = () => {
 
   return (
     <StyledDisclaimer>
-      <b>SummonerLookup</b> isn’t endorsed by <b>Riot Games</b> and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. <b>League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</b>
+      <p>
+        <b>SummonerLookup</b> isn’t endorsed by <b>Riot Games</b> and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. <b>League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</b>
+      </p>
+      <p><b>SummonerLookup</b> is not affiliated with nor endorsed by <a href="http://u.gg" target="_blank" rel="noopener noreferrer"><b>U.gg</b></a>. Wherever there's a <UggLogo style={{ width: "14px", transform: "translateY(30%)" }} /> logo, that indicates which data comes from <a href="http://u.gg" target="_blank" rel="noopener noreferrer"><b>U.gg</b></a>.</p>
     </StyledDisclaimer>
   );
 };
--- client/src/components/atoms/Disclaimer/Disclaimer.styled.js
+++ client/src/components/atoms/Disclaimer/Disclaimer.styled.js
@@ -1,9 +1,17 @@
 import styled from "styled-components";
+import { FlexColStart } from "../FlexBoxes/FlexBoxes.styled";
 
-export const StyledDisclaimer = styled.p`
-    font-size: .8rem;
-    color: #cccccc;
-    font-weight: 400;
+export const StyledDisclaimer = styled(FlexColStart)`
+
+    gap: 1.5rem;
     padding: 2rem;
-    max-width: 1920px;
+    
+    p {
+        text-align: left;
+        width: 100%;
+        font-size: .8rem;
+        color: #cccccc;
+        font-weight: 400;
+        max-width: 1920px;
+    }
 `;
