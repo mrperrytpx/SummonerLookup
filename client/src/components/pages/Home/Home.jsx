0de30f90b0cd54a9bb2b4979541f98614029d63f
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-07-03 11:21:47 +0200

    updated rank crests

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -5,57 +5,57 @@ import { SearchSummoner } from "../../organisms/SearchSummoner/SearchSummoner";
 import { useScreenSize } from "../../../hooks/useScreenSize";
 import { Background } from "components/atoms/Background/Background";
 import {
-  FlexColCenter,
-  FlexRowSpaceBetween,
-  FlexCol,
+    FlexColCenter,
+    FlexRowSpaceBetween,
+    FlexCol,
 } from "components/atoms/FlexBoxes/FlexBoxes.styled";
 import { LinkButton } from "components/atoms/LinkButton/LinkButton";
 import { Span } from "components/atoms/Span/Span";
 
 export const Home = () => {
-  const { width } = useScreenSize();
+    const { width } = useScreenSize();
 
-  return (
-    <StyledHome center={width >= 710 ? true : false}>
-      {width >= 710 ? (
-        <>
-          <Background format="video" />
-          <SearchSummoner />
-          <FlexColCenter gap="1rem">
-            <Span align="center">Example Summoner:</Span>
-            <FlexRowSpaceBetween flow="wrap" gap="1rem">
-              <LinkButton
-                minwidth="120px"
-                variant="secondary"
-                to="/eun1/perryx"
-              >
-                Perryx
-              </LinkButton>
-            </FlexRowSpaceBetween>
-          </FlexColCenter>
-        </>
-      ) : (
-        <>
-          <Background format="image" />
-          <FlexColCenter
-            gap="1rem"
-            style={{ width: "100%", marginTop: "1rem" }}
-          >
-            <CompactSearchSummoner />
-            <SquareLogo fill="white" width="200" />
-            <FlexCol gap="1rem">
-              <Span align="center">Example Summoner:</Span>
-              <LinkButton
-                minwidth="120px"
-                variant="secondary"
-                to="/eun1/perryx"
-              >
-                Perryx
-              </LinkButton>
-            </FlexCol>
-          </FlexColCenter>
-        </>
-      )}
-    </StyledHome>
-  );
+    return (
+        <StyledHome center={width >= 710 ? true : false}>
+            {width >= 710 ? (
+                <>
+                    <Background format="video" />
+                    <SearchSummoner />
+                    <FlexColCenter gap="1rem">
+                        <Span align="center">Example Summoner:</Span>
+                        <FlexRowSpaceBetween flow="wrap" gap="1rem">
+                            <LinkButton
+                                minwidth="120px"
+                                variant="secondary"
+                                to="/eun1/perryx"
+                            >
+                                Perryx
+                            </LinkButton>
+                        </FlexRowSpaceBetween>
+                    </FlexColCenter>
+                </>
+            ) : (
+                <>
+                    <Background format="image" />
+                    <FlexColCenter
+                        gap="1rem"
+                        style={{ width: "100%", marginTop: "1rem" }}
+                    >
+                        <CompactSearchSummoner />
+                        <SquareLogo fill="white" width="200" />
+                        <FlexCol gap="1rem">
+                            <Span align="center">Example Summoner:</Span>
+                            <LinkButton
+                                minwidth="120px"
+                                variant="secondary"
+                                to="/eun1/perryx"
+                            >
+                                Perryx
+                            </LinkButton>
+                        </FlexCol>
+                    </FlexColCenter>
+                </>
+            )}
+        </StyledHome>
+    );
 };
