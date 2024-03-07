4bbd5128b1191528636139de16f79859f24b4c6d
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-03-07 17:28:37 +0100

    pnpm stuff

--- server/index.js
+++ server/index.js
@@ -2,5 +2,10 @@ const app = require("./server");
 
 // Start server
 app.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${process.env.PORT ?? 3001}`);
+    console.log(
+        `Server listening on port ${process.env.PORT ?? 3001} ${
+            process.env.NODE_ENV === "development" &&
+            `- http://localhost:${process.env.PORT}`
+        }`
+    );
 });
--- server/utils/connectToMongoAtlas.js
+++ server/utils/connectToMongoAtlas.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectToMongoAtlas = () => {
-    mongoose.connect(
-        process.env.MONGO_CONNECT,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        (err) => err ? console.log(err) : console.log("Connected to Cluster0")
-    );
+    mongoose
+        .connect(process.env.MONGO_CONNECT, {})
+        .then(() => console.log("Connected to Cluster0"));
 };
 
 module.exports = connectToMongoAtlas;
