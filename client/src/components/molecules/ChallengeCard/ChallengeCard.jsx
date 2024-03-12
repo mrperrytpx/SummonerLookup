f7f77a8135bab59737bfc20dd7d497edbded6570
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-03-12 16:50:24 +0100

    Challenge cards swap to Intl.DateTimeFormat api

--- client/src/components/molecules/ChallengeCard/ChallengeCard.jsx
+++ client/src/components/molecules/ChallengeCard/ChallengeCard.jsx
@@ -9,43 +9,54 @@ import { FlexCol, FlexRow } from "components/atoms/FlexBoxes/FlexBoxes.styled";
 import { useIntersectionObserver } from "hooks/useIntersectionObserver";
 
 export const ChallengeCard = ({ challenge }) => {
+    const queryClient = useQueryClient();
+    const { server } = useParams();
+    const leagueChallengesData = queryClient.getQueryData(["challenges"]);
+    const challengeData = leagueChallengesData.find((chal) => chal.id === challenge.challengeId);
 
-  const queryClient = useQueryClient();
-  const { server } = useParams();
-  const leagueChallengesData = queryClient.getQueryData(["challenges"]);
-  const challengeData = leagueChallengesData.find(chal => chal.id === challenge.challengeId);
+    const challengeRef = useRef(null);
+    const isVisible = useIntersectionObserver(challengeRef, { rootMargin: "200px" });
 
-  const challengeRef = useRef(null);
-  const isVisible = useIntersectionObserver(challengeRef, { rootMargin: "200px" });
+    return (
+        <StyledChallengeCard isVisible={isVisible} ref={challengeRef} tier={challenge?.level}>
+            <FlexRow gap="0.5rem">
+                {isVisible && (
+                    <ImageContainer
+                        data-challenge-badge
+                        src={`https://ddragon.leagueoflegends.com/cdn/img/challenges-images/${challenge?.challengeId}-${
+                            challenge?.level === "NONE" ? "IRON" : challenge?.level
+                        }.png`}
+                        alt="Challenge badge"
+                        width="46px"
+                        radius="50%"
+                        min="46px"
+                    />
+                )}
+                <FlexCol data-info gap="0.25rem">
+                    <div>
+                        <Span width="auto" size="m">
+                            <b>{challengeData.name + " "}</b>
+                        </Span>
+                        <Span width="auto" size="sm">
+                            - {challengeData.description}
+                        </Span>
+                    </div>
 
-  return (
-    <StyledChallengeCard isVisible={isVisible} ref={challengeRef} tier={challenge?.level}>
-      <FlexRow gap="0.5rem">
-        {isVisible && (
-          <ImageContainer
-            data-challenge-badge
-            src={`https://ddragon.leagueoflegends.com/cdn/img/challenges-images/${challenge?.challengeId}-${challenge?.level === "NONE" ? "IRON" : challenge?.level}.png`}
-            alt="Challenge badge"
-            width="46px"
-            radius="50%"
-            min="46px"
-          />
-        )}
-        <FlexCol data-info gap="0.25rem">
-          <div>
-            <Span width="auto" size="m"><b>{challengeData.name + " "}</b></Span>
-            <Span width="auto" size="sm">- {challengeData.description}</Span>
-          </div>
-
-          <Span size="s">
-            <em>
-              {challenge?.position ? `#${challenge.position} - ` : null}
-              better than {Math.round(((1 - challenge.percentile) * 100))}% of {SERVER_VALUES[server]} - <b>{challenge.level}</b> tier
-            </em>
-          </Span>
-          <Span width="auto" align="right" size="xs">{new Date(challenge.achievedTime).toDateString()}</Span>
-        </FlexCol>
-      </FlexRow>
-    </StyledChallengeCard>
-  );
+                    <Span size="s">
+                        <em>
+                            {challenge?.position ? `#${challenge.position} - ` : null}
+                            better than {Math.round((1 - challenge.percentile) * 100)}% of {SERVER_VALUES[server]} -{" "}
+                            <b>{challenge.level}</b> tier
+                        </em>
+                    </Span>
+                    <Span width="auto" align="right" size="xs">
+                        {new Intl.DateTimeFormat("en-GB", {
+                            dateStyle: "long",
+                            timeStyle: "short",
+                        }).format(challenge.achievedTime)}
+                    </Span>
+                </FlexCol>
+            </FlexRow>
+        </StyledChallengeCard>
+    );
 };
